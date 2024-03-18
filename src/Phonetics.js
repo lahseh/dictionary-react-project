97e7f48c5539de4850eea15be3f84497be8b482a
lahseh <orisaleyeblessing@gmail.com>
2024-03-18 22:44:04 +0000

    added photo component and styled

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
   return (
     <div className="Phonetics">
-      <a href={props.phonetics.audio} target="_blank">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
       <span className="text">{props.phonetics.text}</span>
--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Search.css";
 import Definition from "./Definition";
+import Photos from "./Photos";
+import "./Search.css";
 
 export default function Search(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [definition, setDefinition] = useState("");
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setDefinition(response.data[0]);
+  }
+
+  function handlePhotoResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentation: https://api.dictionaryapi.dev/api/v2/entries/en/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let photoApiKey = "46fc06b4af0o785a8td60a716534ff63";
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photoApiKey}`;
+
+    axios.get(photoApiUrl).then(handlePhotoResponse);
   }
 
   function handleSubmit(event) {
@@ -31,27 +42,22 @@ export default function Search(props) {
     setLoaded(true);
     search();
   }
-
   if (loaded) {
     return (
       <div className="Search">
         <section>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              placeholder="Search for a word"
-              onChange={handleKeyword}
-            />
-            <div className="hint">
-              Suggested words: power, money, singer, prayer, sunset...
-            </div>
-            <Definition definition={definition} />
+            <input type="search" onChange={handleKeyword} />
           </form>
+          <div className="hint">Suggested words: sunset, wine, yoga....</div>
         </section>
+        <Definition definition={definition} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
     load();
-    return "Loading";
+    return "loading";
   }
 }
